ba88c0bb91148b32e1c60c05d185bb74f1aabde6
Yonatan Salmon <yonatansalmon@gmail.com>
2019-12-17 15:51:56 +0200

    local storage

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import Navbar from "./components/Navbar"
 import TextBox from "./components/TextBox"
 import Profile from "./components/Profile"
 import TweetList from "./components/TweetList"
+import ls from 'local-storage'
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tweets: []
+      tweets: [window.localStorage.getItem("Tweet")]
     }
 
   }
 
+
   onHandleTweet = (tweet) => {
+    let savedTweets = this.state.tweets
+    savedTweets.push(tweet)
+    window.localStorage.setItem("Tweet", savedTweets)
+    console.log(savedTweets)
     this.setState((prevState) => {
       return { tweets: [tweet, ...prevState.tweets] }
     })
